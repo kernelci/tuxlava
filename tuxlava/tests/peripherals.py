e912a60627257944b86a66b4c96c9857a7664f17
Vishal Bhoj <vishal.bhoj@linaro.org>
2024-09-12 16:50:46 +0530

    Add sysfs interface tests
    Signed-off-by: Vishal Bhoj <vishal.bhoj@linaro.org>

--- tuxlava/tests/peripherals.py
+++ tuxlava/tests/peripherals.py
@@ -12,13 +12,21 @@ from tuxlava.tests import Test
 class Peripherals(Test):
     devices = ["qemu-*", "fvp-aemva", "avh-imx93", "avh-rpi4b", "fastboot-*"]
     need_test_definition = True
+    branch = "master"
 
     def render(self, **kwargs):
         kwargs["name"] = self.name
         kwargs["timeout"] = self.timeout
+        kwargs["branch"] = self.branch
         return self._render("peripherals.yaml.jinja2", **kwargs)
 
 
-class PeripheralsUSBGadgetFramwork(Peripherals):
-    name = "usb-gadget-framwork"
+class PeripheralsUSBGadgetFramework(Peripherals):
+    name = "usb-gadget-framework"
     timeout = 10
+
+
+class PeripheralsysfsFramework(Peripherals):
+    name = "sysfs-interface-framework"
+    timeout = 10
+    branch = "interface"
