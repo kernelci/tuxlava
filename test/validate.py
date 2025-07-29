868143c3713dd9a0cb4617647b2f5be31d9e2349
Alok Ranjan <alok.ranjan@linaro.org>
2025-07-29 12:42:53 +0530

    test: validate.py: update lava-dispatcher
    Update lava-dispatcher version to '2025.06.dev0075'. This is in sync
    with the lava-dispatcher version which is currently being used in
    tuxrun. Since tuxrun has dependency on tuxlava keeping lava-dispatcher
    versions same will unify things and make validation of lava
    job-definition files more consistent.
    
    Signed-off-by: Alok Ranjan <alok.ranjan@linaro.org>

--- test/validate.py
+++ test/validate.py
@@ -7,11 +7,14 @@ import sys
 from pathlib import Path
 
 BASE = (Path(__file__) / "..").resolve()
+IMAGE = "registry.gitlab.com/lava/lava/amd64/lava-dispatcher:2025.06.dev0075"
 
 
 def main():
     client = docker.from_env()
     print(client.version()["Version"])
+    print("Pulling LAVA DISPATCHER:", IMAGE)
+    client.images.pull(IMAGE)
     print("Checking for LAVA validity")
     directory_path = BASE / "unit" / "refs" / "definitions"
     print(directory_path)
@@ -19,7 +22,7 @@ def main():
     for filename in files:
         print(f"{filename=}")
         container = client.containers.run(
-            image="registry.gitlab.com/lava/lava/amd64/lava-dispatcher:2024.09.dev0167",
+            image=IMAGE,
             command="/usr/share/lava-common/lava-schema.py job /data/%s" % filename,
             volumes={"%s" % directory_path: {"bind": "/data", "mode": "rw"}},
             detach=True,
