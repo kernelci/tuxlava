123169c1ab643f99323f45844014ef237652fec0
Ben Copeland <ben.copeland@linaro.org>
2025-11-24 17:13:23 +0000

    devices: add ampereone-ac04 and altra-max-ac02
    This add two server types AmpereOne AC04 and Altra Max AC02
    
    Signed-off-by: Ben Copeland <ben.copeland@linaro.org>

--- tuxlava/devices/nfs.py
+++ tuxlava/devices/nfs.py
@@ -210,6 +210,14 @@ class NfsAmpereOne(NfsDevice):
     rootfs = "https://storage.tuxboot.com/debian/trixie/arm64/rootfs.tar.xz"
 
 
+class NfsAltraMaxAc02(NfsAmpereOne):
+    name = "nfs-altra-max-ac02"
+
+
+class NfsAmpereOneOc04(NfsAmpereOne):
+    name = "nfs-ampereone-ac04"
+
+
 class NfsOrionO6(NfsDevice):
     name = "nfs-orion-o6"
 
